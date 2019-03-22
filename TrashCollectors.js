787a14187e573f14e35eb870663ebd937e136cdc
Eiim <ethan@we3chapmans.com>
2019-03-22 09:49:56 -0400

    Test with gem blockx

--- TrashCollectors.js
+++ TrashCollectors.js
@@ -20,11 +20,13 @@ func:function()
 		gizmos:true,
 		modes:{
 			'food removal':{name:'Remove spoiled food',icon:[3,7,3,7],desc:'Remove 100 [spoiled food].'},
-			'water removal':{name:'Remove muddy water',icon:[8,6,8,6],desc:'Remove 100 [muddy water].'}
+			'water removal':{name:'Remove muddy water',icon:[8,6,8,6],desc:'Remove 100 [muddy water].'},
+			'gem block removal':{name:'Remove gem blocks',icon:[18,8,18,8],desc:'Remove 100 [gem block]s',req:{G.getRes('gem block')["visible"]:true}}
 		},
 		effects:[
 			{type:'convert',from:{'spoiled food':100},into:{'spoiled food':0},every:1,repeat:1,mode:'food removal'},
 			{type:'convert',from:{'muddy water':100},into:{'muddy water':0},every:1,repeat:1,mode:'water removal'},
+			{type:'convert',from:{'gem block':100},into:{'gem block':0},every:1,repeat:1,mode:'gem block removal'},
 			{type:'mult',value: 2,req:{'cleanliness':true}}
 		],
 		req:{'garbage collection':true},
